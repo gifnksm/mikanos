88623f77bc14734d060236f7030e6fa5c2274c2b
gifnksm <makoto.nksm+github@gmail.com>
2021-05-03 16:05:15 +0900

    第16章

--- kernel/terminal.hpp
+++ kernel/terminal.hpp
@@ -16,10 +16,12 @@
 class Terminal {
 public:
   static const int kRows = 15, kColumns = 60;
+  static const int kLineMax = 128;
 
   Terminal();
   unsigned int LayerId() const { return layer_id_; }
   Rectangle<int> BlinkCursor();
+  Rectangle<int> InputKey(uint8_t modifier, uint8_t keycode, char ascii);
 
 private:
   std::shared_ptr<ToplevelWindow> window_;
@@ -28,6 +30,18 @@ private:
   Vector2D<int> cursor_{0, 0};
   bool cursor_visible_{false};
   void DrawCursor(bool visible);
+  Vector2D<int> CalcCursorPos() const;
+
+  int linebuf_index_{0};
+  std::array<char, kLineMax> linebuf_{};
+  void Scroll1();
+
+  void ExecuteLine();
+  void Print(const char *s);
+
+  std::deque<std::array<char, kLineMax>> cmd_history_{};
+  int cmd_history_index_{-1};
+  Rectangle<int> HistoryUpDown(int direction);
 };
 
 void TaskTerminal(uint64_t task_id, int64_t data);
